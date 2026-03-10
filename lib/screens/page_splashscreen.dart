6bc6e66d4da2df25b49f481bc1809080c6ceaaa2
Kurniawan20 <kurniawanrisky551@gmail.com>
2026-03-10 14:46:58 +0700

    feat: implement animated splash screen with app version checking, maintenance, update handling, and initial navigation.

--- lib/screens/page_splashscreen.dart
+++ lib/screens/page_splashscreen.dart
@@ -126,7 +126,7 @@ class _SplashScreenState extends State<SplashScreen>
     _mainController.forward();
 
     // Minimum Splash Duration
-    await Future.delayed(const Duration(milliseconds: 1500));
+    await Future.delayed(const Duration(milliseconds: 2500));
 
     if (mounted) {
       setState(() => _animationComplete = true);
